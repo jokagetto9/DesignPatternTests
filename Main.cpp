092396677af51b76687584459bb3decc287053fd
jokagetto9 <jokagetto9@gmail.com>
2016-04-23 13:38:37 -0600

    Added RapidJSON?
    tested out rapid json...barely

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,25 @@
 #include "lib.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+
+using namespace rapidjson;
+
 //********************************* MAIN *********************************
 int main(int argc, char* args[]){
-	cout << "Hello World" <<endl;
-	int a;
-	cin >> a;
-	return 0;
+	char *json;
+	stringstream temp;
+	ifstream file ("test.json");
+	if (file.is_open())  {
+		temp << file.rdbuf();
+		json = (char*)temp.str().c_str();
+		file.close();
+
+		Document d;
+		d.Parse(json);
+		int a;
+		cin >> a;
+		return 0;
+	} 
 }//*/
