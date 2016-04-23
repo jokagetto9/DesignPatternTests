15202c6d2879769135f4c4cea9664fd058f31471
jokagetto9 <jokagetto9@gmail.com>
2016-04-22 23:32:06 -0600

    Hello World
    Blank Slate

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include "lib.h"
+//********************************* MAIN *********************************
+int main(int argc, char* args[]){
+	cout << "Hello World" <<endl;
+	int a;
+	cin >> a;
+	return 0;
+}//*/
---
+++ lib.h
@@ -0,0 +1,16 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+//#include <cassert>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+#endif
